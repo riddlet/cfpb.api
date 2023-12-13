4f25e11ba7cebce5b2b0fede1c3b9dc11a99b1f5
triddle <travis.riddle@cfpb.gov>
2023-12-13 15:53:38 -0500

    fix product & issue aggregation

--- R/query_aggregates.R
+++ R/query_aggregates.R
@@ -56,7 +56,7 @@
 #'   query_complaints(size = 1)
 #' }
 #'
-query_aggregates <- function(search_term = NULL, agg_type = 'state', field = NULL,
+query_aggregates <- function(search_term = NULL, agg_type = 'state', field = 'complaint_what_happened',
                              company = NULL, company_public_response = NULL,
                              company_received_max = NULL, company_received_min = NULL,
                              company_response = NULL, consumer_consent_provided = NULL,
@@ -66,65 +66,82 @@ query_aggregates <- function(search_term = NULL, agg_type = 'state', field = NUL
                              submitted_via = NULL, tags = NULL, timely = NULL,
                              zip_code = NULL)
 {
-  if (!is.null(field) & !is.null(search_term))
+  if (missing(search_term))
   {
-    cat(paste0("Searching for ", search_term, " in ", field, "\n"))
-  } else if (!is.null(field))
-  {
-    cat(paste0("Searching for ", search_term, " in complaint_what_happened\n"))
+    stop('Search term required')
   }
 
+  cat(paste0("Searching for '", search_term, "' in ", field, "\n"))
 
   cfpb_query_list <- as.list(match.call(expand.dots = FALSE))[-1]
   cfpb_query_list <- lapply(cfpb_query_list, eval.parent, n = 2)
-  cfpb_query_path <- httr::modify_url(
-    url = get_cfpb_url(),
-    path = get_cfpb_url_path("geo/states"),
-    query = cfpb_query_list
-  )
-  res <- httr::GET(cfpb_query_path)
-
-  if (res$status_code == get_success_code())
-  {
-    text_res <- jsonlite::fromJSON(httr::content(res, "text", encoding = "UTF-8"))
-  } else if (res$status_code == get_invalid_status_value())
-  {
-    cat(cfpb_query_path, "\n")
-    stop(paste("Invalid status value.  HTTP return code:", res$status_code))
-  } else
-  {
-    cat(cfpb_query_path, "\n")
-    stop(paste("HTTP return code:", res$status_code))
-  }
 
   if (agg_type == 'product')
   {
-    products <- text_res$aggregations$product$product$buckets$key
-    subproducts <- text_res$aggregations$product$product$buckets$product$buckets
-    outdat <- dplyr::bind_rows(subproducts)
-    outdat$product <- rep(products, sapply(subproducts, nrow))
-    outdat$subproduct <- outdat$key
-    outdat <- outdat[,c('product', 'subproduct', 'doc_count')]
-    return(outdat)
+    cfpb_query_path <- httr::modify_url(
+      url = get_cfpb_url(),
+      path = get_cfpb_url_path(""),
+      query = cfpb_query_list
+    )
+
+    res <- httr::GET(cfpb_query_path)
+    if (check_response_status(res, cfpb_query_path)) {
+      text_res <- jsonlite::fromJSON(httr::content(res, "text", encoding = "UTF-8"))
+      products <- text_res$aggregations$product$product$buckets$key
+      sub_prod_list <- text_res$aggregations$product$product$buckets$sub_product.raw$buckets
+      outdat <- dplyr::bind_rows(sub_prod_list)
+      outdat$product <- rep(products, sapply(sub_prod_list, nrow))
+      outdat$subproduct <- outdat$key
+      outdat <- outdat[,c('product', 'subproduct', 'doc_count')]
+      missing_prods <- text_res$aggregations$product$product$buckets
+      missing_prods <- missing_prods[!missing_prods$key %in% outdat$product,]
+      missing_prods$product <- missing_prods$key
+      missing_prods$subproduct <- missing_prods$key
+      missing_prods <- missing_prods[,c('product', 'subproduct', 'doc_count')]
+      outdat <- rbind(outdat, missing_prods)
+    }
   } else if (agg_type == 'issue')
   {
-    issues <- text_res$aggregations$issue$issue$buckets$key
-    subissues <- text_res$aggregations$issue$issue$buckets$issue$buckets
-    outdat <- dplyr::bind_rows(subissues)
-    outdat$issue <- rep(issues, sapply(subissues, nrow))
-    outdat$subissue <- outdat$key
-    outdat <- outdat[,c('issue', 'subissue', 'doc_count')]
-    return(outdat)
+    cfpb_query_path <- httr::modify_url(
+      url = get_cfpb_url(),
+      path = get_cfpb_url_path(""),
+      query = cfpb_query_list
+    )
+
+    res <- httr::GET(cfpb_query_path)
+    if (check_response_status(res, cfpb_query_path)) {
+      text_res <- jsonlite::fromJSON(httr::content(res, "text", encoding = "UTF-8"))
+      issues <- text_res$aggregations$issue$issue$buckets$key
+      subissues <- text_res$aggregations$issue$issue$buckets$sub_issue.raw$buckets
+      outdat <- dplyr::bind_rows(subissues)
+      outdat$issue <- rep(issues, sapply(subissues, nrow))
+      outdat$subissue <- outdat$key
+      outdat <- outdat[,c('issue', 'subissue', 'doc_count')]
+      missing_issues <- text_res$aggregations$issue$issue$buckets
+      missing_issues <- missing_issues[!missing_issues$key %in% outdat$issue,]
+      missing_issues$issue <- missing_issues$key
+      missing_issues$subissue <- missing_issues$key
+      missing_issues <- missing_issues[,c('issue', 'subissue', 'doc_count')]
+      outdat <- rbind(outdat, missing_issues)
+    }
   } else if (agg_type == 'state')
   {
-    outdat <- data.frame(
-      state = text_res$aggregations$state$state$buckets$key,
-      doc_count = text_res$aggregations$state$state$buckets$doc_count
+    cfpb_query_path <- httr::modify_url(
+      url = get_cfpb_url(),
+      path = get_cfpb_url_path("geo/states"),
+      query = cfpb_query_list
     )
-    return(outdat)
+    res <- httr::GET(cfpb_query_path)
+    if (check_response_status(res, cfpb_query_path)){
+      text_res <- jsonlite::fromJSON(httr::content(res, "text", encoding = "UTF-8"))
+      outdat <- data.frame(
+        state = text_res$aggregations$state$state$buckets$key,
+        doc_count = text_res$aggregations$state$state$buckets$doc_count
+      )
+    }
   } else
   {
     stop("Unknown aggregation type. Specify 'state', 'product', or 'issue'.")
   }
-
+  return(outdat)
 }
